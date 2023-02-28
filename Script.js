0ab91a0be6100c3e79fce573e697525d453235b1
Nishita Upadhyay <91163759+nishita84@users.noreply.github.com>
2023-02-27 22:23:15 -0500

    Implementing the fetch() function in JS

--- Script.js
+++ Script.js
@@ -1,4 +1,35 @@
+const API_URL = "https://jacintodesign.github.io/quotes-api/data/quotes.json";
+
+async function getAPI(url) {
+  const response = await fetch(url);
+  var data = await response.json();
+  console.log(data);
+
+  if (response) {
+    hideloader();
+  }
+  show(data);
+}
+
+getAPI(API_URL);
+function hideloader() {
+  alert("loading");
+}
+
 function myFunction() {
   var response = "Hi";
   alert(response);
+}
+
+function show(data) {
+  let tab = `<tr>
+          <th>Name</th>
+          <th>Quote</th>
+         </tr>`;
+  for (let r of data.list) {
+    tab += `<tr> 
+    <td>${r.name} </td>
+    <td>${r.quote}</td>          
+</tr>`;
+  }
 }
